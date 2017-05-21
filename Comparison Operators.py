0f9ed8c764f676bf4a47292f8f379c563d4ea7ea
sapal6 <satyabrata.pal1@gmail.com>
2017-05-21 14:25:40 +0530

    Python_tutorials_comparisonOperators

---
+++ Comparison Operators.py
@@ -0,0 +1,22 @@
+# Comparison operators are used to compare two values
+# Comparison operators return a True or a False
+
+# Some Types of Comparison opearators -->
+# == Equal to
+# != Not equal to
+# < Less than
+# > greater than
+# <= Less than or equal to
+# >= Greater than or equal to
+
+
+# Examples of Comparison operators -->
+
+print (1 == 2) # returns a False
+
+print('a' == 'b') # returns a False
+
+print(45 > 34) # returns a True
+
+print(56 <= 56) # returns a True
+
