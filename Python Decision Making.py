0216df861f93c771d5e49a8d26d2482b437260b9
sapal6 <satyabrata.pal1@gmail.com>
2017-05-28 21:37:56 +0530

    Add files via upload

---
+++ Python Decision Making.py
@@ -0,0 +1,38 @@
+# Python decision making is done by using a conditional statement like the If statement
+# An If statement consists of 3 parts -->
+# If keyword
+# a conditional operator
+# the values which needs comparison
+
+# An If condition always returns a True or a False
+
+# Let's Dive into some Action now-->
+
+# Let's find if alex is a Vampire ? ? -->
+
+# Age of Alex-->
+
+age = 100
+
+if age >= 0 or age <= 99:
+    print("Alex is a human")
+else:
+    print("OMG !!!! Alex is a Vampire")
+
+
+# Let's put more conditions to our code to be sure that Alex is really a Vampire
+
+if age >= 0 and age <=99:
+    print("Alex is a human")
+elif age <= 101:
+    print("Alex might be a long lived human")
+else:
+    print("OMG!!! Alex is really a Vampire")
+
+# Let's run the code now to see if Alex is a Vampire
+
+
+
+
+
+
